74cf04af27eb4ad6bd7f638c0b262746576b08e8
ddabahc <ddabahc89@gmail.com>
2022-02-28 13:15:52 -0600

    navbar design done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+import Header from "./components/nav/Header";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
