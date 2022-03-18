5ce4528c09a3b8532b071d3336384eb9655d57dc
ddabahc <ddabahc89@gmail.com>
2022-03-18 11:50:47 -0600

    ratings and product page done :rocket:

--- src/components/cards/ProductCard.js
+++ src/components/cards/ProductCard.js
@@ -4,6 +4,7 @@ import React from "react";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import image from "../../images/image.png";
 import { Link } from "react-router-dom";
+import { showAverage } from "../../functions/rating";
 
 const { Meta } = Card;
 
@@ -11,29 +12,37 @@ const ProductCard = ({ product }) => {
   const { images, title, description, slug } = product;
 
   return (
-    <Card
-      cover={
-        <img
-          src={images && images.length ? images[0].url : image}
-          style={{ height: "150px", objectFit: "cover" }}
-          className="p-1"
+    <>
+      {product && product.ratings && product.ratings.length > 0 ? (
+        showAverage(product)
+      ) : (
+        <div className="text-center pt-1 pb-3">No Rating Yet</div>
+      )}
+
+      <Card
+        cover={
+          <img
+            src={images && images.length ? images[0].url : image}
+            style={{ height: "150px", objectFit: "cover" }}
+            className="p-1"
+          />
+        }
+        actions={[
+          <Link to={`/product/${slug}`}>
+            <EyeOutlined className="text-warning" /> <br /> View Product
+          </Link>,
+          <>
+            <ShoppingCartOutlined className="text-danger" /> Add to Cart
+          </>,
+        ]}
+      >
+        {" "}
+        <Meta
+          title={title}
+          description={`${description && description.substring(0, 40)}...`}
         />
-      }
-      actions={[
-        <Link to={`/product/${slug}`}>
-          <EyeOutlined className="text-warning" /> <br /> View Product
-        </Link>,
-        <>
-          <ShoppingCartOutlined className="text-danger" /> Add to Cart
-        </>,
-      ]}
-    >
-      {" "}
-      <Meta
-        title={title}
-        description={`${description && description.substring(0, 40)}...`}
-      />
-    </Card>
+      </Card>
+    </>
   );
 };
 
